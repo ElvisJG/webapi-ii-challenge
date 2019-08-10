5bc4afdec85cce5f265de3dee4983d1f36259559
Elvis Gonzalez <ElvisJGonzalez@gmail.com>
2019-08-09 20:24:29 -0400

    added singular comments using ID

--- data/posts-router.js
+++ data/posts-router.js
@@ -34,8 +34,17 @@ router.get('/:id', async (req, res) => {
   }
 });
 
+// GET /api/posts/{id}/comments
 router.get('/:id/comments', async (req, res) => {
   try {
+    const { id } = req.params;
+    const comments = await Posts.findCommentById(id);
+
+    comments
+      ? res.status(200).json(comments)
+      : res
+          .status(404)
+          .json({ message: 'The post with the specified ID does not exist' });
   } catch (error) {
     // Log error
     console.log(error);
